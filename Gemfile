d3b12d13e92cb68f3a0a8b7c8f013b5d8819a5fd
wormwood <wormwood@technocalypse.net>
2013-06-04 19:07:00 -0400

    Rendering json

--- Gemfile
+++ Gemfile
@@ -1,16 +1,21 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'rack-cache'
+gem "json"
+gem "multi_json"
+gem 'thin'
+gem "pg"
+gem "activerecord", "~> 4.0.0.rc1"
 gem 'sinatra-activerecord'
-gem 'mysql'
-gem 'json'
-
+gem 'awesome_print'
+gem 'tux'
 
 group :test do
   gem 'rspec'
   gem 'rack-test'
   gem 'shoulda'
   gem 'sqlite3'
-  gem 'activeresource' # to test the service
+  gem 'activeresource', "~> 4.0.0.beta1" # to test the service
 end
--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,27 @@
 require_relative 'base'
 
 class MyApp < SinatraRecord
+  require 'sinatra/activerecord'
+  require 'sinatra/json'
+  require 'json'
+  require 'multi_json'
+
+  db = URI.parse('postgres://vagrant:vagrant@localhost/sinatra')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+
+  class Note < ActiveRecord::Base
+  end
 
   get '/' do
-    render 'HelloWorld!'
+    { :foo => :bar }.to_json
   end
 
 end
